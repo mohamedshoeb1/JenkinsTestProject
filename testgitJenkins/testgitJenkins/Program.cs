3cad71c45b2ba7e6605481bd6ced2f4e6a18545d
Mohamed Shoeb <mshoeb@uplandsoftware.com>
2021-05-05 17:12:55 +0200

    first commit

---
+++ testgitJenkins/testgitJenkins/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace testgitJenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Build Succeed!");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
